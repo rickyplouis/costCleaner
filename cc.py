6fcd90ea518f3fe040bec75a63f7020a83523082
rickyplouis <ricardoplouis@gmail.com>
2018-12-11 13:22:10 -0600

    added cost type descriptions

--- cc.py
+++ cc.py
@@ -2,6 +2,15 @@ import pandas as pd
 from tabulate import tabulate
 testPath = 'input.xlsx'
 
+costCodes = {
+    1: 'Material',
+    2: 'Labor',
+    3: 'Equipment',
+    4: 'Subcontractors',
+    5: 'Other',
+    6: 'Ready Mix'
+}
+
 def cleanSpreadsheet(filepath):
     # import dat
     data = pd.ExcelFile(filepath)
@@ -27,19 +36,25 @@ def getCostByMonth(df):
     return df.resample('M')['Cost'].sum()
 
 def getCostByType(df):
-    return df.groupby(['Cost Type'])['Cost'].sum()
+    newDF = df.replace({'Cost Type': costCodes})
+    return newDF.groupby(['Cost Type'])['Cost'].sum()
 
 def getCostByMonthAndType(df):
     return df.groupby([df.index.month, 'Cost Type'])['Cost'].sum()
 
-df0 = cleanSpreadsheet(testPath)
-df1 = getCostByType(df0)
-df2 = getCostByMonth(df0)
-df3 = getCostByMonthAndType(df0)
-#print(getCostByMonth(newDF))
+def writeToExcel(df):
+    df0 = cleanSpreadsheet(testPath)
+    df1 = getCostByType(df0)
+    df2 = getCostByMonth(df0)
+    df3 = getCostByMonthAndType(df0)
+    writer = pd.ExcelWriter('output.xlsx')
+    df1.to_excel(writer, 'Sheet1')
+    df2.to_excel(writer, 'Sheet2')
+    df3.to_excel(writer, 'Sheet3')
+    writer.save()
+    return
+
+df00 = cleanSpreadsheet(testPath)
+print(getCostByType(df00))
+#print(getCostByType(df00))
 #print(getCostByType(newDF))
-writer = pd.ExcelWriter('output.xlsx')
-df1.to_excel(writer, 'Sheet1')
-df2.to_excel(writer, 'Sheet2')
-df3.to_excel(writer, 'Sheet3')
-writer.save()
