8fc89a1d294d7bef321422a88b3b2693cd8d086b
rickyplouis <ricardoplouis@gmail.com>
2018-12-13 12:44:50 -0600

    Month analysis

--- main.py
+++ main.py
@@ -5,6 +5,21 @@ from tabulate import tabulate
 import analysis
 testPath = 'input.xlsx'
 
+monthCodes = {
+    1: 'January',
+    2: 'February',
+    3: 'March',
+    4: 'April',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'August',
+    9: 'September',
+    10: 'October',
+    11: 'November',
+    12: 'December'
+}
+
 def cleanSpreadsheet(filepath):
     # import dat
     data = pd.ExcelFile(filepath)
@@ -56,12 +71,17 @@ def cashFormat(num):
     return '${:,.2f}'.format(num)
 
 def percentageFormat(num):
-    return int(num * 100)
+    return round(num * 100, 2)
 
 def maxOrMinText(df, isMax):
     row = getMaxOrMinCost(df, isMax)
     typeOfCost = 'highest' if isMax else 'lowest'
-    return 'The ' + typeOfCost + ' cost was ' + row.name + ' at ' + str(cashFormat(row['Cost'])) + ' which equals ' + str(percentageFormat(row['Cost %'])) + '% of total costs'
+    return 'The ' + typeOfCost + ' cost was for ' + row.name + ' at ' + str(cashFormat(row['Cost'])) + ' which equals ' + str(percentageFormat(row['Cost %'])) + '% of total costs'
+
+def maxOrMinMonth(df, isMax):
+    row = getMaxOrMinCost(df, isMax)
+    typeOfCost = 'highest' if isMax else 'lowest'
+    return 'The ' + typeOfCost + ' cost was for the month of ' + monthCodes[row.name] + ' at ' + str(cashFormat(row['Cost'])) + ' which equals ' + str(percentageFormat(row['Cost %'])) + '% of total costs'
 
 def sumText(df):
     totalCost = df['Cost'].sum()
@@ -88,7 +108,10 @@ typeSum = sumText(typeDF)
 typeAvg = avgText(typeDF)
 
 monthSum = sumText(monthDF)
+monthMax = maxOrMinMonth(monthDF, True)
+monthMin = maxOrMinMonth(monthDF, False)
 
 costByTypeText = ['Cost By Type', typeSum, typeAvg, typeMax, typeMin]
-costByMonthText = ['Cost By Month', monthSum]
+costByMonthText = ['Cost By Month', monthSum, monthMax, monthMin]
 writeTemplate(typeDF, costByTypeText)
+writeTemplate(monthDF, costByMonthText)
