e6edde832381ea5c02c50a747c6e361767c61920
rickyplouis <ricardoplouis@gmail.com>
2018-12-13 17:11:02 -0600

    Linter configuration

--- analysis.py
+++ analysis.py
@@ -1,3 +1,4 @@
+"""Provide tools for analyzing dataframes."""
 import pandas as pd
 costCodes = {
     1: 'Material',
@@ -8,22 +9,30 @@ costCodes = {
     6: 'Ready Mix'
 }
 
+
 def createCostPercentageCol(df):
+    """Append a column to dataframe with cost percentages."""
     totalCost = df['Cost'].sum()
     df['Cost %'] = df.apply(lambda x: x.Cost / totalCost, axis=1)
     return df
 
+
 def getCostByMonth(df):
+    """Sort dataframe costs by month."""
     newDF = df.groupby([df.index.month])['Cost'].sum()
     newDF.index.rename('Month', inplace=True)
     return createCostPercentageCol(pd.DataFrame(newDF))
 
+
 def getCostByType(df):
+    """Sort dataframe costs by Type/CostCode."""
     newDF = df.replace({'Cost Type': costCodes})
     newDF = newDF.groupby(['Cost Type'])['Cost'].sum()
     return createCostPercentageCol(pd.DataFrame(newDF))
 
+
 def getCostByMonthAndType(df):
+    """Sort dataframe costs by Month and Type/CostCode."""
     newDF = df.replace({'Cost Type': costCodes})
     newDF = newDF.groupby([df.index.month, 'Cost Type'])['Cost'].sum()
     newDF.index.rename(['Month', 'Cost Type'], inplace=True)
